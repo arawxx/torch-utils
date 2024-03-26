930a2165b2215eb3ddec9bbd0f9376056c8d9a1a
Arash Hajian nezhad <arash.hajiannezhad@gmail.com>
2024-03-26 04:37:35 +0330

    add support for layer-wise learning rate decay

--- torchutils/schedulers/schedulers.py
+++ torchutils/schedulers/schedulers.py
@@ -14,13 +14,13 @@ class CosineAnnealingLinearWarmup(_LRScheduler):
     Implements a warmup cosine annealing learning rate scheduler.
 
     Attributes:
-        warmup_epochs (int): The number of warmup epochs.
-        max_epochs (int): The total number of epochs.
-        initial_lr (float): The initial learning rate.
-        cosine_annealing_epochs (int): The number of epochs for the cosine annealing phase.
+        `warmup_epochs` (int): The number of warmup epochs.
+        `max_epochs` (int): The total number of epochs.
+        `initial_lr` (float): The initial learning rate.
+        `cosine_annealing_epochs` (int): The number of epochs for the cosine annealing phase.
 
     Methods:
-        get_lr(): Get the learning rate for each parameter group.
+        `get_lr()`: Get the learning rate for each parameter group.
     """
 
     def __init__(
@@ -28,22 +28,22 @@ class CosineAnnealingLinearWarmup(_LRScheduler):
         optimizer: Optimizer,
         warmup_epochs: int,
         max_epochs: int,
-        initial_lr: float = 1e-6,
+        minimum_lr: float = 1e-6,
         last_epoch: int = -1,
     ) -> None:
         """
         Initialize a new WarmupCosineAnnealingLR instance.
 
         Args:
-            optimizer (torch.optim.Optimizer): The optimizer for which to schedule the learning rate.
-            warmup_epochs (int): The number of warmup epochs.
-            max_epochs (int): The total number of epochs.
-            initial_lr (float, optional): The initial learning rate. Default is 1e-6.
-            last_epoch (int, optional): The index of the last epoch. Default is -1.
+            `optimizer` (torch.optim.Optimizer): The optimizer for which to schedule the learning rate.
+            `warmup_epochs` (int): The number of warmup epochs.
+            `max_epochs` (int): The total number of epochs.
+            `minimum_lr` (float, optional): The lowest learning rate the scheduler starts from. Default is 1e-6.
+            `last_epoch` (int, optional): The index of the last epoch. Default is -1.
         """
         self.warmup_epochs = warmup_epochs
         self.max_epochs = max_epochs
-        self.initial_lr = initial_lr
+        self.initial_lr = minimum_lr
         self.cosine_annealing_epochs = self.max_epochs - self.warmup_epochs
 
         super().__init__(optimizer, last_epoch)
@@ -56,7 +56,14 @@ class CosineAnnealingLinearWarmup(_LRScheduler):
             List[float]: The learning rate for each parameter group.
         """
         if self.last_epoch < self.warmup_epochs:
-            return [self.initial_lr + (base_lr - self.initial_lr) * (self.last_epoch) / self.warmup_epochs for base_lr in self.base_lrs]
+            lr = [self.initial_lr + (base_lr - self.initial_lr) * (self.last_epoch) / self.warmup_epochs for base_lr in self.base_lrs]
         else:
             cos_input = math.pi * (self.last_epoch - self.warmup_epochs) / self.cosine_annealing_epochs
-            return [base_lr * (1 + math.cos(cos_input)) / 2 for base_lr in self.base_lrs]
+            lr = [base_lr * (1 + math.cos(cos_input)) / 2 for base_lr in self.base_lrs]
+
+        # Applying lr_scale if it exists in the parameter group (in case of layer-wise learning rate decay usage)
+        for i, param_group in enumerate(self.optimizer.param_groups):
+            if 'lr_scale' in param_group:
+                lr[i] *= param_group['lr_scale']
+
+        return lr
